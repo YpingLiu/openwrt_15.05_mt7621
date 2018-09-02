a431ddf2ff10fce8091cfb314721f6aa2e0222ef
liuyiping <liuyiping@t-road.cn>
2018-09-02 12:24:55 +0800

    1.Changing LAN to DHCP mode is equivalent to changing router to switch 2.Change LAN mode so that PHP can be configured on a PC when the PC and board are in the same network segment

--- package/base-files/files/lib/functions/uci-defaults.sh
+++ package/base-files/files/lib/functions/uci-defaults.sh
@@ -211,11 +211,12 @@ ucidef_set_interface_lan() {
 set network.lan='interface'
 set network.lan.ifname='$ifname'
 set network.lan.force_link=1
-set network.lan.type='bridge'
-set network.lan.proto='static'
-set network.lan.ipaddr='192.168.1.1'
-set network.lan.netmask='255.255.255.0'
-set network.lan.ip6assign='60'
+set network.lan.proto='dhcp'
+#set network.lan.type='bridge'
+#set network.lan.proto='static'
+#set network.lan.ipaddr='192.168.1.1'
+#set network.lan.netmask='255.255.255.0'
+#set network.lan.ip6assign='60'
 EOF
 }
 
